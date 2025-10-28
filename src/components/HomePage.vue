8f195ebe701f3b4ef3962672f85a9c3e7b79e0d4
Sai Htoo Lwin <saihtoolwin66@gmail.com>
2025-10-28 16:58:30 +0630

    fix home page padding

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,38 +1,38 @@
 <template>
-  <div class="container mx-auto px-4 grid grid-cols-1 lg:grid-cols-2 gap-10">
+  <div class="container min-h-[65vh] md:min-h-[70vh] mx-auto px-4 grid grid-cols-1 lg:grid-cols-2 lg:gap-10">
     <!-- Human cartoon coding (iframe) -->
-    <div class="w-full h-full hidden lg:flex items-center justify-center order-2  ">
+    <div class="w-full  hidden lg:flex items-center justify-center order-1 lg:order-2 ">
       <iframe src="https://lottie.host/embed/9e839c39-2403-4ff3-9b98-98e7797475a8/3sk4cDu71b.json"
-        class="w-full aspect-[14/9] border-none" allowfullscreen></iframe>
+        class="w-full md:aspet-[12/9] lg:aspect-[12/9] border-none lg:mb-28" ></iframe>
     </div>
 
     <!-- Personal introduction and social icons -->
-    <div class="md:order-1 flex flex-col h-full">
+    <div class="md:order-1 flex flex-col  ">
       <!-- Text content wrapper -->
       <div class="flex-grow flex flex-col justify-center text-center md:text-left">
         <div class="text-white dark:text-gray-900">
-          <p class="text-2xl sm:text-3xl md:text-3xl mb-4">Hi, I'm</p>
+          <p class="text-xl sm:text-2xl md:text-3xl mb-4">Hi, I'm</p>
           <p
-            class="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl text-white dark:text-gray-900 hover:text-secondary transition-all duration-300 cursor-pointer">
+            class="text-4xl sm:text-5xl md:text-6xl lg:text-7xl  text-white dark:text-gray-900 hover:text-secondary transition-all duration-300 cursor-pointer">
             Sai Htoo Lwin.
           </p>
         </div>
-        <div class="mt-4 text-base sm:text-lg md:text-xl lg:text-2xl text-white dark:text-gray-900">
+        <div class="mt-2 text-sm md:text-md lg:text-lg text-white dark:text-gray-900">
           I'm a passionate
-          <span class="text-red-400 text-lg sm:text-xl md:text-2xl textShadow">full-stack developer</span>
-          with a love for creating dynamic and responsive web applications. With experience in both front-end and
-          back-end technologies, I excel in turning complex requirements into user-friendly solutions.
+          <span class="text-red-400  textShadow">full-stack developer</span>
+          with 2 years of experience and a love for creating dynamic and responsive web applications. I have experience
+          working in both front-end and back-end technologies, and I excel in turning complex requirements into
+          user-friendly solutions. I can also collaborate effectively in a team environment.
         </div>
       </div>
 
       <!-- Social Icons -->
-      <div class="flex justify-center md:justify-start mt-8 2xl:mb-20 space-x-5 text-white dark:text-gray-900">
-        <i class="fa-brands fa-square-facebook fa-xl hover-target"></i>
-        <i class="fa-brands fa-instagram fa-xl hover-target"></i>
-        <!-- <i class="fa-solid fa-envelope fa-xl hover-target"></i> -->
-        <i class="fa-brands fa-linkedin fa-xl hover-target"></i>
-        <i class="fa-brands fa-github fa-xl hover-target"></i>
-      </div>
+     <div class="flex justify-center md:justify-start mt-8 lg:mt-0 2xl:mb-20 space-x-5 text-white dark:text-gray-900">
+  <i class="fa-brands fa-square-facebook text-base md:text-lg lg:text-xl hover-target"></i>
+  <i class="fa-brands fa-instagram text-base md:text-xl lg:text-2xl hover-target"></i>
+  <i class="fa-brands fa-linkedin text-base md:text-xl lg:text-2xl hover-target"></i>
+  <i class="fa-brands fa-github text-base md:text-xl lg:text-2xl hover-target"></i>
+</div>
     </div>
 
   </div>
