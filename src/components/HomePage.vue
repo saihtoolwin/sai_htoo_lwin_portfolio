ddbd840959ef4443b9ce558a7b7b320b7328bf0b
Sai Htoo Lwin <saihtoolwin66@gmail.com>
2025-10-28 11:57:49 +0630

    text responsive done

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,37 +1,40 @@
 <template>
-  <div class="container mx-auto px-4 grid grid-cols-1 md:grid-cols-2 gap-10">
+  <div class="container mx-auto px-4 grid grid-cols-1 lg:grid-cols-2 gap-10">
     <!-- Human cartoon coding (iframe) -->
-    <div class="w-full h-full flex items-center justify-center md:order-2">
+    <div class="w-full h-full hidden lg:flex items-center justify-center order-2  ">
       <iframe src="https://lottie.host/embed/9e839c39-2403-4ff3-9b98-98e7797475a8/3sk4cDu71b.json"
-        style="width: 100%; height: 500px; border: none" class="mb-20" allowfullscreen></iframe>
+        class="w-full aspect-[14/9] border-none" allowfullscreen></iframe>
     </div>
 
     <!-- Personal introduction and social icons -->
-    <div class=" md:order-1 flex flex-col h-full">
-      <div class="flex-grow flex flex-col justify-center">
+    <div class="md:order-1 flex flex-col h-full">
+      <!-- Text content wrapper -->
+      <div class="flex-grow flex flex-col justify-center text-center md:text-left">
         <div class="text-white dark:text-gray-900">
-          <p class="text-3xl mb-8">Hi, I'm</p>
+          <p class="text-2xl sm:text-3xl md:text-3xl mb-4">Hi, I'm</p>
           <p
-            class="text-8xl text-white dark:text-gray-900 hover:text-secondary transition-all duration-300 hover:cursor-pointer">
+            class="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl text-white dark:text-gray-900 hover:text-secondary transition-all duration-300 cursor-pointer">
             Sai Htoo Lwin.
           </p>
         </div>
-        <div class="text-xl text-white dark:text-gray-900">
+        <div class="mt-4 text-base sm:text-lg md:text-xl lg:text-2xl text-white dark:text-gray-900">
           I'm a passionate
-          <span class="text-red-400 text-2xl textShadow">full-stack developer</span>
-          with a love for creating dynamic and responsive web applications. With
-          experience in both front-end and back-end technologies, I excel in
-          turning complex requirements into user-friendly solutions.
+          <span class="text-red-400 text-lg sm:text-xl md:text-2xl textShadow">full-stack developer</span>
+          with a love for creating dynamic and responsive web applications. With experience in both front-end and
+          back-end technologies, I excel in turning complex requirements into user-friendly solutions.
         </div>
       </div>
-      <div class="flex align-bottom text-white dark:text-gray-900  space-x-5">
+
+      <!-- Social Icons -->
+      <div class="flex justify-center md:justify-start mt-8 2xl:mb-20 space-x-5 text-white dark:text-gray-900">
         <i class="fa-brands fa-square-facebook fa-xl hover-target"></i>
         <i class="fa-brands fa-instagram fa-xl hover-target"></i>
-        <i class="fa-solid fa-envelope fa-xl hover-target"></i>
+        <!-- <i class="fa-solid fa-envelope fa-xl hover-target"></i> -->
         <i class="fa-brands fa-linkedin fa-xl hover-target"></i>
         <i class="fa-brands fa-github fa-xl hover-target"></i>
       </div>
     </div>
+
   </div>
 </template>
 <script setup></script>
